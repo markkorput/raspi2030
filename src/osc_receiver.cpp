23e6decc36e2cc21c876aad9e8ebc99d4bfde82c
Mark <dr.theman@gmail.com>
2016-04-26 22:33:28 +0200

    OscReceiver class source

---
+++ src/osc_receiver.cpp
@@ -0,0 +1,197 @@
+//
+//  osc_receiver.cpp
+//  emptyExample
+//
+//  Created by Mark van de Korput on 16-04-19.
+//
+//
+
+#include "ofMain.h"
+#include "osc_receiver.hpp"
+#include "ofxJSONElement.h"
+
+using namespace of2030;
+
+void OscReceiver::configure(unsigned int port, Interface* interface){
+    m_port = port;
+
+    if(interface)
+        m_interface = interface;
+
+    if(m_bConnected){
+        // reconnect
+        connect();
+    }
+}
+
+void OscReceiver::setup(){
+    if(m_interface == NULL){
+        m_interface = Interface::instance();
+    }
+
+    if(!m_bConnected){
+        connect();
+    }
+}
+
+void OscReceiver::update(){
+    int message_count = 0;
+
+    // check for waiting messages
+    while(m_oscReceiver.hasWaitingMessages() && message_count < MAX_MESSAGES_PER_CYCLE){
+        // get the next message
+        ofxOscMessage m;
+        m_oscReceiver.getNextMessage(m);
+        message_count++;
+
+        // "/change"-type message?
+        // if(m.getAddress() == "/change"){
+        //     ofLogVerbose() << "Got /change OSC Message";
+        //     processChangeMessage(m);
+        //     continue;
+        // }
+
+        if(m.getAddress() == "/effect"){
+            ofLogVerbose() << "Got /effect OSC Message";
+            processEffectMessage(m);
+            continue;
+        }
+
+        if(m.getAddress() == "/message"){
+            ofLogVerbose() << "Got /message OSC Message";
+            processMessageMessage(m);
+            continue;
+        }
+
+        ofLog() << "Got unknown OSC Message " << m.getAddress();
+    }
+}
+
+void OscReceiver::destroy(){
+    if(m_bConnected){
+        disconnect();
+    }
+}
+
+bool OscReceiver::connect(){
+    m_oscReceiver.enableReuse();
+
+    if(m_oscReceiver.setup(m_port)){
+        m_bConnected = true;
+        ofLog() << "of2030::OscReceiver listening to port: " << m_port;
+        return true;
+    }
+
+    ofLogWarning() << "OscReceiver could not start listening to port: " << m_port;
+    return false;
+}
+
+void OscReceiver::disconnect(){
+    m_bConnected = false;
+}
+
+void OscReceiver::processEffectMessage(ofxOscMessage &m){
+    if(m.getNumArgs() < 1){
+        ofLogWarning() << "/effect message didn't have any args";
+        return;
+    }
+
+    if(m.getArgType(0) != OFXOSC_TYPE_STRING){
+        ofLogWarning() << "/effect didn't have string arg";
+        return;
+    }
+
+    effects::Effect* effect = createEffectFromJsonString(m.getArgAsString(0));
+    if(effect){
+        // ofLog() << "[OscReceiver] Triggering interface's effectEvent";
+        ofNotifyEvent(m_interface->effectEvent, *effect, m_interface);
+        return;
+    }
+
+    ofLogWarning() << "Could not create effect instance from OSC /effect message with data: " << m.getArgAsString(0);
+}
+
+void OscReceiver::getMapFromJsonString(const std::string &str, map<string, string> &target){
+    ofxJSONElement jsonEl;
+    jsonEl.parse(str);
+
+    // get all attributes as string (no support for nester structure for now)
+    vector<string> attrs = jsonEl.getMemberNames();
+
+    for(int i=attrs.size()-1; i>=0; i--){
+        string name = attrs[i];
+        target[name] = jsonEl[name].asString();
+    }
+}
+
+effects::Effect* OscReceiver::createEffectFromJsonString(const std::string &json_string){
+    effects::Effect* pEffect;
+    std::string type, value;
+
+    ofxJSONElement json;
+    json.parse(json_string);
+
+    if(json.isMember("type")){
+        type = json["type"].asString();
+    }
+
+    // create an instance of the appropriate effects class
+    // beased on the model's type attribute
+    // and populate type-specific attributes
+    // the assign the pEffect pointer to the created instance
+    if(type == "OFF"){
+
+        effects::Off* effect = new effects::Off();
+        pEffect = (effects::Effect*)effect;
+
+    } else if(type == "COLOR"){
+
+        effects::Color* effect = new effects::Color();
+        if(json.isMember("r") && json.isMember("g") && json.isMember("b")){
+            effect->color.set(ofColor(json["r"].asInt(),
+                                      json["g"].asInt(),
+                                      json["b"].asInt()));
+        }
+        pEffect = (effects::Effect*)effect;
+
+    } else {
+
+        ofLogWarning() << "[EffectCreator] got unknown effect model type: " << type;
+        pEffect = new effects::Effect();
+
+    }
+
+    // process some more (optional) general effect attributes
+    if(json.isMember("start"))
+        pEffect->startTime = json["start"].asFloat();
+
+    if(json.isMember("end"))
+        pEffect->endTime = json["end"].asFloat();
+
+    if(json.isMember("duration"))
+        pEffect->duration = json["duration"].asFloat();
+
+    return pEffect;
+}
+
+void OscReceiver::processMessageMessage(ofxOscMessage &m){
+    if(m.getNumArgs() < 1){
+        ofLogWarning() << "/message message didn't have any args";
+        return;
+    }
+
+    if(m.getArgType(0) != OFXOSC_TYPE_STRING){
+        ofLogWarning() << "/effect didn't have string arg";
+        return;
+    }
+
+    string messageType = m.getArgAsString(0);
+
+    if(messageType == "cursor"){
+        effects::Cursor* cursor_effect = new effects::Cursor();
+        ofNotifyEvent(m_interface->effectEvent, (*(effects::Effect*)cursor_effect), m_interface);
+        return;
+    }
+
+    ofLogWarning() << "Unknown messageType" << messageType;
+}
---
+++ src/osc_receiver.hpp
@@ -0,0 +1,51 @@
+//
+//  osc_receiver.hpp
+//  emptyExample
+//
+//  Created by Mark van de Korput on 16-04-19.
+//
+//
+
+#ifndef osc_receiver_hpp
+#define osc_receiver_hpp
+
+//#include <stdio.h>
+#include "ofxOsc.h"
+
+#include "interface.hpp"
+#include "effects.hpp"
+
+namespace of2030 {
+
+    class OscReceiver {
+        const static int MAX_MESSAGES_PER_CYCLE = 10;
+
+    public:
+        OscReceiver() : m_port(2030), m_interface(NULL), m_bConnected(false){}
+        ~OscReceiver(){ destroy(); }
+        void configure(unsigned int port=2030, Interface* interface=NULL);
+        void setup();
+        void update();
+        void destroy();
+
+        unsigned int getPort(){ return m_port; };
+
+    private: // methods
+        bool connect();
+        void disconnect();
+
+        void processEffectMessage(ofxOscMessage &m);
+        void processMessageMessage(ofxOscMessage &m);
+
+        void getMapFromJsonString(const std::string &str, map<string, string> &map);
+        effects::Effect* createEffectFromJsonString(const std::string &json_string);
+
+    private: // attributes
+        Interface* m_interface;
+        ofxOscReceiver m_oscReceiver;
+        bool m_bConnected;
+        unsigned int m_port;
+    };
+}
+
+#endif /* osc_receiver_hpp */
