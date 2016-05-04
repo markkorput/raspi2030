d2be3735a2eaabc83447d90bf2f2e9ebb884ea64
Mark van de Korput <dr.theman@gmail.com>
2016-05-04 21:22:03 +0200

    changed window settings again, now properly or raspi 2

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 
 //========================================================================
 int main( ){
-	ofGLWindowSettings settings;
-	settings.setGLVersion(3,2);
+	// ofGLWindowSettings settings;
+	// settings.setGLVersion(3,2);
+	ofGLESWindowSettings settings;
+	settings.glesVersion = 2;
 	ofCreateWindow(settings);
 	// ofSetupOpenGL(1024,768, OF_WINDOW); // OF_FULLSCREEN
 	ofRunApp( new ofApp());
