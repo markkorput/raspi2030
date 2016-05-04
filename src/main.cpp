2010b97a46be891c40d51db6bc796c10851ee03c
Mark van de Korput <dr.theman@gmail.com>
2016-05-04 21:02:40 +0200

    change window settings to support GL shaders

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,9 @@
 
 //========================================================================
 int main( ){
-
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3,2);
+	ofCreateWindow(settings);
+	// ofSetupOpenGL(1024,768, OF_WINDOW); // OF_FULLSCREEN
 	ofRunApp( new ofApp());
-
 }
