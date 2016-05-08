c046cac10fd1e4d1e3c9399933bfe01a05ef3bef
Mark <dr.theman@gmail.com>
2016-05-08 11:30:27 +0000

    custom raspi code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+#include "shared2030.h"
+
 //========================================================================
 int main(int argc, char** argv){
-	ofSetupOpenGL(768,576, OF_WINDOW);			// <-------- setup the GL context
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
+#ifdef __APPLE__
+  ofSetupOpenGL(768,576, OF_WINDOW);      // <-------- setup the GL context
+#else // raspbery pi
+  // ofGLWindowSettings settings;
+  // settings.setGLVersion(3,2);
+  ofGLESWindowSettings settings;
+  settings.glesVersion = 2;
+  ofCreateWindow(settings);
+  // ofSetupOpenGL(1024,768, OF_WINDOW); // OF_FULLSCREEN
+#endif // __APPLE__
+
+  // this kicks off the running of my app
+  // can be OF_WINDOW or OF_FULLSCREEN
+  // pass in width and height too:
+  ofRunApp(new ofApp());
 }
